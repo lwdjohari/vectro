8941b474436a7081a3ab6c0c39e92dca55ddd8fd
lwdjohari <linggawasistha.djohari@outlook.com>
2025-05-24 20:37:40 +0700

    Fix Catch2 include on rawbuffer_test.cc

--- tests/frame/rawbuffer/rawbuffer_test.cc
+++ tests/frame/rawbuffer/rawbuffer_test.cc
@@ -1,4 +1,5 @@
-#include <catch2/catch_test_macros.hpp>
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch_all.hpp"
 #include "vectro/frame/raw_buffer.h"  // adjust include path if needed
 #include <cstdlib>
 
