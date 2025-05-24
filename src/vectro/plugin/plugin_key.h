c2a98ca7f011835ceb7105e9a1fbcc72e46430b2
lwdjohari <linggawasistha.djohari@outlook.com>
2025-05-24 15:36:35 +0700

    Add PluginKeys impl

---
+++ src/vectro/plugin/plugin_key.h
@@ -0,0 +1,14 @@
+#pragma once
+// Developer-defined plugin keys
+namespace vectro {
+namespace plugin {
+namespace PluginKeys {
+inline constexpr char kRateLimiter[] = "ratelimiter";
+inline constexpr char kOverloadShedder[] = "shedder";
+inline constexpr char kSessionTagger[] = "tagger";
+inline constexpr char kInterceptor[] = "interceptor";
+inline constexpr char kHeartbeatEmitter[] = "heartbeat";
+}  // namespace PluginKeys
+}  // namespace plugin
+
+}  // namespace vectro
